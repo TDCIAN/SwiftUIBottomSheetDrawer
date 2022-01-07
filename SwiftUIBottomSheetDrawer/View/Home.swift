9ed0c0341db1185b021e0f2e437ed1de8e161311
TDCIAN <tdcian71@gmail.com>
2022-01-07 11:45:31 +0900

    add blur effect

--- SwiftUIBottomSheetDrawer/View/Home.swift
+++ SwiftUIBottomSheetDrawer/View/Home.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct Home: View {
+    // Search text binding value
+    @State var searchText: String = ""
+    
+    // Gesture properties
+    @State var offset: CGFloat = 0
+    @State var lastOffset: CGFloat = 0
+    @GestureState var gestureOffset: CGFloat = 0
+    
     var body: some View {
         ZStack {
             // For getting frame for image
@@ -21,6 +29,7 @@ struct Home: View {
                     .frame(width: frame.width, height: frame.height)
                 
             }
+            .blur(radius: getBlurRadius())
             .ignoresSafeArea()
             
             // For getting height for drag gesture
@@ -31,12 +40,66 @@ struct Home: View {
                     ZStack {
                         // Bottom Sheet
                         BlurView(style: .systemThinMaterialDark)
+                            .clipShape(CustomCorner(corners: [.topLeft, .topRight], radius: 30))
+                        
+                        VStack {
+                            Capsule()
+                                .fill(Color.white)
+                                .frame(width: 60, height: 4)
+                                .padding(.top)
+                            
+                            TextField("Search", text: $searchText)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal)
+                                .background(BlurView(style: .dark))
+                                .cornerRadius(10)
+                                .colorScheme(.dark)
+                                .padding(.top, 10)
+                            
+                            // ScrollView content
+                        }
+                        .padding(.horizontal)
+                        .frame(maxHeight: .infinity, alignment: .top)
                     }
+                    .offset(y: height - 100)
+                        .offset(y: -offset > 0 ? -offset <= (height - 100) ? offset : -(height - 100) : 0)
+                    .gesture(DragGesture().updating($gestureOffset, body: { value, state, transcation in
+                        state = value.translation.height
+                        onChange()
+                    }).onEnded({ value in
+                        let maxHeight = height - 100
+                        withAnimation {
+                            // Loginc conditions for moving states
+                            // Up down or mid
+                            if -offset > 100 && -offset < maxHeight / 2 {
+                                // Mid
+                                offset = -(maxHeight / 3)
+                            } else if -offset > maxHeight / 2 {
+                                offset = -maxHeight
+                            } else {
+                                offset = 0
+                            }
+                        }
+                        // Storing last offset, so that the gesture can continue from the last position
+                        lastOffset = offset
+                    }))
+                        
                 )
             }
             .ignoresSafeArea(.all, edges: .bottom)
-
         }
+    }
+    
+    func onChange() {
+        DispatchQueue.main.async {
+            self.offset = gestureOffset + lastOffset
+        }
+    }
+    
+    // Blur radius for background
+    func getBlurRadius() -> CGFloat {
+        let progress = -offset / (UIScreen.main.bounds.height - 100)
+        return progress * 30
     }
 }
 
